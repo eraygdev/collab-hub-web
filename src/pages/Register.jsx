bc2b3c58afdd0fdba965334eeb592cfb4b90f840
Eray Günlü <88250653+eraygdev@users.noreply.github.com>
2026-09-22 19:35:50 +0300

    .

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -22,7 +22,7 @@ export default function Register() {
   };
 
   return (
-    <div className="w-full flex-1 flex items-center justify-center px-4 sm:px-6 lg:px-8 py-12 sm:py-16">
+    <div className="w-full flex-1 flex items-center justify-center px-4 sm:px-6 lg:px-8 py-6 sm:py-12 lg:py-16">
       <div className="w-full max-w-md">
 
         {/* Başlık */}
